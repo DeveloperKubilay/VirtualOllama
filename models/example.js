beefe2abec81423565e6ca5f35f2c3821a2eff6c
DeveloperKubilay <107062509+DeveloperKubilay@users.noreply.github.com>
2025-08-07 17:54:21 +0300

    feat: add example chat model implementation

--- models/example.js
+++ models/example.js
@@ -4,8 +4,7 @@ module.exports = {
   "genericName": "Android",  //Example: "Qwen"
   "name": "AndroidProMax",   //Example: "qwen2-2b",
   "license": "Apache-2.0",   //NOT REQUIRED
-  "gerenicPromt": "You are a helpful assistant.", //NOT REQUIRED
-  "parameter_size": "3B", //NOT REQUIRED
+  "parameter_size": "3B",    //NOT REQUIRED
   "chat": async function(Chat, Lastmsg, Messages, options) {
     Chat("Selam ")
     await setTimeout(1000); // Simulate processing delay
